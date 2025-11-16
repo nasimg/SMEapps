8828cc82edca46b5c677a31d749d2647e2d0af9b
abaziz1k99 <abdulaziz1k99@outlook.com>
2025-11-16 12:26:35 +0600

    Add Module Model

---
+++ SMEapps/SMEapps.Shared/Model/Module.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMEapps.Shared.Model
+{
+    public class Module
+    {
+        public int ModuleId { get; set; }
+
+        [Required(ErrorMessage ="Title is required")]
+        public string Title { get; set; }
+
+        public string ModuleName { get; set; }
+
+        public string IconName { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Class { get; set; }
+
+        public string GroupTitle { get; set; }
+        [Required(ErrorMessage = "Status is required")]
+        public int? Status { get; set; }
+        [Required(ErrorMessage = "Menu Position is required")]
+        public int? MenuPosition { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime? DateCreated { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
